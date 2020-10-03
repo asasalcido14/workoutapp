f82a294e44c5a2c64f2f969893398aec56a02279
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-03 08:29:28 -0700

    updated routes 2

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+router.post("/api/workouts", (req,res) => {
+    Workout.create({})
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
       .catch(err => {
-        res.status(400).json(err);
+        res.json(err);
       });
   });
 
@@ -21,9 +21,29 @@ router.post("/api/workouts", ({ body }, res) => {
         res.json(dbWorkout);
       })
       .catch(err => {
-        res.status(400).json(err);
+        res.json(err);
       });
   });
+
+router.get("/api/workouts",(req,res) => {
+    Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err =>{
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    Workout.find({}).limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
 
 
